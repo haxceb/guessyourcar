d1326778585a6a3cec628c725fe594de9f3052cd
Haseeb <haxcebmalik@yahoo.com>
2021-05-02 18:21:53 +0500

    Homepage Created

---
+++ src/Screens/QuestionsScreen/Questions.js
@@ -0,0 +1,20 @@
+export const Questions = [
+    {
+        questionText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscingelit, Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+        anserOptions: [
+            {
+                answerText: "consectetur adipiscing",
+                isCorrect: false,
+            }, {
+                answerText: "consectetur adipiscing",
+                isCorrect: true
+            }, {
+                answerText: "consectetur adipiscing",
+                isCorrect: false
+            }
+        ],
+        isMulti: false,
+        isFill: false,
+        isDropDown: false
+    }
+]
